393f03ea1248a44948be904240ba7589227b9257
antelmor <andres.tellez.m13@gmail.com>
2024-02-13 13:36:09 -0500

    Merging by reconstructing the exchange tensor

--- TB2J/io_merge.py
+++ TB2J/io_merge.py
@@ -1,103 +1,73 @@
 import os
 import copy
 import numpy as np
+from itertools import combinations_with_replacement, product
 from TB2J.io_exchange import SpinIO
 
-I = np.eye(3)
-uz = np.array([[0.0, 0.0, 1.0]])
+uy = np.array([0.0, 1.0, 0.0])
+uz = np.array([0.0, 0.0, 1.0])
 
-def get_rotation_matrix(magmoms):
+def get_Jani_coefficients(a):
 
-    dim = magmoms.shape[0]
-    v = magmoms / np.linalg.norm(magmoms, axis=-1).reshape(-1, 1)
-    n = v[:, [1, 0, 2]]
-    n[:, 0] *= -1
-    n[:, -1] *= 0
-    n /= np.linalg.norm(n, axis=-1).reshape(dim, 1)
-    z = np.repeat(uz, dim, axis=0)
-    A = np.stack([z, np.cross(n, z), n], axis=1)
-    B = np.stack([v, np.cross(n, v), n], axis=1)
-    R = np.einsum('nki,nkj->nij', A, B)
+    if len(a) == 1:
+        u = a
+        v = a
+    else:
+        u = a[[0, 0, 1]]
+        v = a[[0, 1, 1]]
+        
+    coefficients = np.hstack([u*v, np.roll(u, -1, axis=-1)*v + np.roll(v, -1, axis=-1)*u])
 
-    Rnan = np.isnan(R)
-    if Rnan.any():
-        nanidx = np.where(Rnan)[0]
-        R[nanidx] = I
-        R[nanidx, 2] = v[nanidx]
+    return coefficients, u, v
 
-    return R
+def get_projections(a, b, tol=1e-2):
 
-def transform_Jani(Jani, Ri, Rj):
+    if np.linalg.matrix_rank([a, b], tol=tol) == 1:
+        projections = np.empty((2, 3))
+        if np.linalg.matrix_rank([a, uy], tol=tol) == 1:
+            projections[0] = np.cross(a, uz)
+        else:
+            projections[0] = np.cross(a, uy)
+        projections[1] = np.cross(a, projections[0])
+    else:
+        projections = np.cross(a, b)
+    projections /= np.linalg.norm(projections, axis=-1).reshape(-1, 1)
 
-    new_Jani = Ri @ Jani @ Rj.T
-    new_Jani[:, 2] *= 0
-    new_Jani[2, :] *= 0
-    new_Jani = Ri.T @ new_Jani @ Rj
-
-    new_Jani = Rj @ new_Jani @ Ri.T
-    new_Jani[:, 2] *= 0
-    new_Jani[2, :] *= 0
-    new_Jani = Rj.T @ new_Jani @ Ri
-
-    return new_Jani.round(8)
-
-def transform_DMI(DMI, Ri, Rj):
-
-    new_DMI = Ri @ DMI
-    new_DMI[2] *= 0
-    new_DMI = Ri.T @ new_DMI
-
-    new_DMI = Rj @ new_DMI
-    new_DMI[2] *= 0
-    new_DMI = Rj.T @ new_DMI
-
-    return new_DMI.round(8)
+    return projections
 
 class SpinIO_merge(SpinIO):
     def __init__(self, *args, **kwargs):
         super(SpinIO_merge, self).__init__(*args, **kwargs)
         self.projv = None
-        self._set_ind_atoms()
 
-    def _set_ind_atoms(self):
+    def _set_projection_vectors(self):
 
-        if not hasattr(self, 'nspin'):
-            self.nspin = len([i for i in self.index_spin if i >= 0])
-        if not self.ind_atoms:
-            self.ind_atoms = dict([(i, i) for i in range(self.nspin)])
-
-    def _set_rotation_matrix(self, reference_cell=None):
-
-        if reference_cell is None:
-            self.sR = np.eye(3)
-        else:
-            self.sR = np.linalg.solve(reference_cell, self.atoms.cell.array)
-
-        self._set_ind_atoms()
         spinat = self.spinat
-        ind_atoms = self.ind_atoms
-        
-        magmoms = spinat[list(ind_atoms.values())]
-        magmoms = magmoms @ self.sR
+        idx = [i for i in self.index_spin if i >= 0]
+        projv = {}
+        for i, j in combinations_with_replacement(range(self.nspin), 2):
+            a, b = spinat[idx][[i, j]]
+            projv[i, j] = get_projections(a, b)
+            projv[j, i] = projv[i, j]
 
-        self.R = get_rotation_matrix(magmoms)
+        self.projv = projv
 
     @classmethod
-    def load_pickle(cls, path='TB2J_results', fname='TB2J.pickle', reference_cell=None):
+    def load_pickle(cls, path='TB2J_results', fname='TB2J.pickle'):
         obj = super(SpinIO_merge, cls).load_pickle(path=path, fname=fname)
-        obj._set_rotation_matrix(reference_cell=reference_cell)
+        obj._set_projection_vectors()
 
         return obj
 
-def read_pickle(path, reference_cell=None):
+def read_pickle(path):
     p1 = os.path.join(path, 'TB2J_results', 'TB2J.pickle')
     p2 = os.path.join(path, 'TB2J.pickle')
     if os.path.exists(p1) and os.path.exists(p2):
         print(f" WARNING!: Both file {p1} and {p2} exist. Use default {p1}.")
     if os.path.exists(p1):
-        ret = SpinIO_merge.load_pickle(os.path.join(path, 'TB2J_results'), reference_cell=reference_cell)
+        ret = SpinIO_merge.load_pickle(os.path.join(path, 'TB2J_results'))
     elif os.path.exists(p2):
-        ret = SpinIO_merge.load_pickle(path, reference_cell=reference_cell)
+        ret = SpinIO_merge.load_pickle(path)
     else:
         raise FileNotFoundError(f"Cannot find either file {p1} or {p2}")
     return ret
@@ -107,31 +77,62 @@ class Merger():
         if method not in ['structure', 'spin']:
             raise ValueError(f"Unrecognized method '{method}'. Available options are: 'structure' or 'spin'.")
         self.method = method
+        self.dat = [read_pickle(path) for path in paths]
 
         if main_path is None:
-            self.main_dat = read_pickle(paths[-1])
+            self.main_dat = copy.deepcopy(self.dat[-1])
         else:
             self.main_dat = read_pickle(main_path)
+            self.dat.append(copy.deepcopy(self.main_dat))
 
-        self.dat = [read_pickle(path, reference_cell=self.main_dat.atoms.cell.array) for path in paths]
-        if main_path is not None:
-            self.dat.append(self.main_dat) 
+        self._set_projv()
+        
+    def _set_projv(self):
+
+        rotated = self.method == 'structure'
+        if rotated:
+            cell = self.main_dat.atoms.cell.array
+            rotated_cells = np.stack(
+                [obj.atoms.cell.array for obj in self.dat], axis=0
+            )
+            R = np.linalg.solve(cell, rotated_cells)
+
+        proju = {}; projv = {}; coeff_matrix = {}; projectors = {};
+        for key in self.main_dat.projv.keys():
+            vectors = [obj.projv[key] for obj in self.dat]
+            if rotated:
+                vectors = [(R[i] @ vectors[i].T).T for i in range(len(R))]
+            coefficients, u, v = zip(*[get_Jani_coefficients(p) for p in vectors])
+            projectors[key] = np.stack(vectors)
+            coeff_matrix[key] = np.vstack(coefficients)
+            proju[key] = u
+            projv[key] = v
+        
+        self.proju = proju
+        self.projv = projv
+        self.coeff_matrix = coeff_matrix
+        self.projectors = projectors
 
     def merge_Jani(self):
         Jani_dict = {}
+        proju = self.proju; projv = self.projv; coeff_matrix = self.coeff_matrix;
         for key in self.main_dat.Jani_dict.keys():
             try:
-                _, i, j = key
-                Jani_list = []
-                for obj in self.dat:
-                    Jani = obj.sR @ obj.Jani_dict[key] @ obj.sR.T
-                    Jani_list.append(transform_Jani(Jani, obj.R[i], obj.R[j]))
-                Jani_list = np.stack(Jani_list)
+                R, i, j = key
+                u = proju[i, j]
+                v = projv[i, j]
+                Jani = np.stack([sio.Jani_dict[key] for sio in self.dat])
+                projections = np.einsum('nmi,nij,nmj->nm', u, Jani, v).flatten()
             except KeyError as err:
                 raise KeyError(
                     "Can not find key: %s, Please make sure the three calculations use the same k-mesh and same Rcut."
                     % err)
-            Jani_dict[key] = np.nan_to_num(np.mean(Jani_list, axis=0, where=Jani_list != 0.0))
+            newJani = np.linalg.lstsq(coeff_matrix[i, j], projections, rcond=1e-2)[0]
+            Jani_dict[key] = np.array([
+                [newJani[0], newJani[3], newJani[5]],
+                [newJani[3], newJani[1], newJani[4]],
+                [newJani[5], newJani[4], newJani[2]]
+            ])
         self.main_dat.Jani_dict = Jani_dict
 
     def merge_Jiso(self):
@@ -144,25 +145,25 @@ class Merger():
                         "Can not find key: %s, Please make sure the three calculations use the same k-mesh and same Rcut."
                         % err)
             Jdict[key] = J
-        self.main_dat.exchange_Jdict = Jdict           
+        self.main_dat.exchange_Jdict = Jdict
+               
 
     def merge_DMI(self):
         dmi_ddict = {}
         if all(obj.has_dmi for obj in self.dat):
+            projectors = self.projectors
             for key in self.main_dat.dmi_ddict.keys():
                 try:
-                    _, i, j = key
-                    DMI_list = []
-                    for obj in self.dat:
-                        DMI = obj.sR @ obj.dmi_ddict[key]
-                        DMI_list.append(transform_DMI(DMI, obj.R[i], obj.R[j]))
-                    DMI_list = np.stack(DMI_list)
+                    R, i, j = key
+                    p = projectors[i, j]
+                    DMI = np.stack([sio.dmi_ddict[key] for sio in self.dat])
+                    projections = np.einsum('nij,nj->ni', p, DMI).flatten()
                 except KeyError as err:
                     raise KeyError(
                         "Can not find key: %s, Please make sure the three calculations use the same k-mesh and same Rcut."
                         % err)
-                DMI = np.nan_to_num(np.mean(DMI_list, axis=0, where=DMI_list != 0.0))
-                dmi_ddict[key] = DMI
+                newDMI = np.linalg.lstsq(np.vstack(p), projections, rcond=1e-2)[0]
+                dmi_ddict[key] = newDMI
             self.main_dat.dmi_ddict = dmi_ddict
 
 def merge(*paths, main_path=None, method='structure', save=True, write_path='TB2J_results'):
